9db08ff7826756ad73abe8191059b73759be5a8b
Walentin <valession@gmail.com>
2023-07-16 18:31:07 +0300

    Добавлен маппер для объекта Транспортное средство

---
+++ src/main/java/com/dictionary/vehicle/vehicle_dictionary/mapper/VehicleToEntityMapper.java
@@ -0,0 +1,11 @@
+package com.dictionary.vehicle.vehicle_dictionary.mapper;
+
+import com.dictionary.vehicle.vehicle_dictionary.entity.VehicleEntity;
+import com.dictionary.vehicle.vehicle_dictionary.model.Vehicle;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface VehicleToEntityMapper {
+    VehicleEntity vehicleToVehicleEntity(Vehicle vehicle);
+    Vehicle entityVehicleToVehicle(VehicleEntity vehicleEntity);
+}

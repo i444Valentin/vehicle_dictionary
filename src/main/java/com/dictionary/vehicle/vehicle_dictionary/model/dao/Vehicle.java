06bac42737224d13d73e3e93c75ed0790828f1ba
Walentin <valession@gmail.com>
2023-07-19 09:46:31 +0300

    Класс теперь представлен как запись

--- src/main/java/com/dictionary/vehicle/vehicle_dictionary/model/dao/Vehicle.java
+++ src/main/java/com/dictionary/vehicle/vehicle_dictionary/model/dao/Vehicle.java
@@ -1,15 +1,5 @@
 package com.dictionary.vehicle.vehicle_dictionary.model.dao;
 
-import lombok.Value;
-
-@Value
-public class Vehicle {
-    Long id;
-    String brand;
-    String model;
-    String category;
-    String number;
-    String type;
-    String manufactured;
-    boolean trailer;
+public record Vehicle(Long id, String brand, String model, String category,
+                      String number, String type, String manufactured, boolean trailer) {
 }
